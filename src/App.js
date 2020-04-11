d8a4b8b45d88ae06b0762e4ab77320b7d94423cb
DreamerIn <chetubathi07@gmail.com>
2020-04-12 00:50:10 +0530

    Updated code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect } from 'react';
+import Pages from "./pages";
+
+function App(){
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+
+    fetch("https://gitconnected.com/v1/portfolio/dreamerin")
+    .then(res => res.json())
+    .then(user => setUser(user));
+  }, []);
+
+  if(!user){
+    return <div/>;
+  }
+
+  return <Pages user = {user}/>;
+}
+
+export default App;
