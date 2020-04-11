4ca30205be6e5e342906e8cd84a5d00788ee4eed
DreamerIn <chetubathi07@gmail.com>
2020-04-12 00:44:33 +0530

    Initial commit

---
+++ build/precache-manifest.28608c23e152f1097868091050a11137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07acaab43b20f2279d2565b937a4035c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2ced8f30b68d816674",
+    "url": "/static/css/2.af452921.chunk.css"
+  },
+  {
+    "revision": "027d8e20e3747806b524",
+    "url": "/static/css/main.1f758cc3.chunk.css"
+  },
+  {
+    "revision": "3d2ced8f30b68d816674",
+    "url": "/static/js/2.b735a226.chunk.js"
+  },
+  {
+    "revision": "8ae8dcd1d63ad08c955dfd4e6a265dcb",
+    "url": "/static/js/2.b735a226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027d8e20e3747806b524",
+    "url": "/static/js/main.2c8d67cf.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
